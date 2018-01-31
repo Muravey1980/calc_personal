0ad0f288c48ba6f3a17c54acc37017566ee0b4d2
Developer Redmine <developer@nowhere.net>
2018-01-31 16:54:38 +0300

    Добавлена проверка, что введенное число является числом, а введенное действие - корректно.

--- console_calc.pas
+++ console_calc.pas
@@ -2,24 +2,28 @@ Program console_calc;
 
 uses math_personal;
 var
-  a,b,res:real;
-  action:string;
+  a,b:real;
+  action,inp:string;
 begin
-  Writeln('¬ведите число:');
-  Readln(a);
-  Writeln('¬ведите действие:');
-  Readln(action);
+  repeat
+    Writeln('¬ведите число:');
+    Readln(inp);
+  until TryStrToFloat(inp,a); 
+  repeat  
+    Writeln('¬ведите действие:');
+    Readln(action);
+  until (action='sqr') or (action='sqrt') or (action='sin') or (action='cos') or (action='tan')
+    or (action='+') or (action='-') or (action='*') or (action='/');
   if (action='sqr') or (action='sqrt') or (action='sin') or (action='cos') or (action='tan') then
-    Writeln(FuncCalc(a,action))
+    Writeln(action+'('+a+') = '+FuncCalc(a,action))
   else
     if (action='+') or (action='-') or (action='*') or (action='/') then
       begin
-        Writeln('¬ведите число:');
-        Readln(b);
-        Writeln(a+' '+action+' '+b+' = ' MathCalc(a,b,action));
+        repeat
+          Writeln('¬ведите число:');
+          Readln(inp);
+        until TryStrToFloat(inp,b); 
+        Writeln(a+' '+action+' '+b+' = '+MathCalc(a,b,action));
       end
     else Writeln('¬ведено неверное действие.')
-    
-  
-  
 end.
