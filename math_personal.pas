b404c5cf407de5422ec764a7fcd5e6de8e558f15
Developer Redmine <developer@nowhere.net>
2018-01-31 12:14:27 +0300

    Добавлены корень квадратный, возведение в квадрат, а также sin, cos и tan

--- math_personal.pas
+++ math_personal.pas
@@ -1,5 +1,8 @@
 Unit math_personal;
 
+type
+  math_func = function (x: real): real;
+
 Function MathCalc(a:real;b:real;action:char):real;
 begin
   case action of
@@ -9,4 +12,18 @@ begin
     '/': result:=a/b;
   end;
 end;
+
+Function FuncCalc(a:real;func:string):real;
+var  
+  f:math_func;
+begin
+  var math_funcs:= new Dictionary<string,math_func>;
+  math_funcs.Add('sqr',sqr);
+  math_funcs.Add('sqrt',sqrt);
+  math_funcs.Add('sin',sin);
+  math_funcs.Add('cos',cos);
+  math_funcs.Add('tan',tan);
+  result:=math_funcs.Get(func)(a);
+end;
+
 end.
